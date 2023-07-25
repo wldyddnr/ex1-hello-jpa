cfc9a20c091bc2350c72c0051bf9396c00262426
yonguk <wldyddnr@gmail.com>
2023-07-25 18:07:17 +0900

    프록시 getReference()

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,15 +18,24 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Member member = new Member();
-            member.setUsername("user");
-            member.setCreatedBy("ji");
-            member.setCreatedDate(LocalDateTime.now());
+            Member member1 = new Member();
+            member1.setUsername("ji");
 
-            em.persist(member);
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("ji");
+
+            em.persist(member2);
 
             em.flush();
             em.clear();
+
+//            Member findMember = em.find(Member.class, member.getId());
+            Member m1 = em.find(Member.class, member1.getId());
+            Member m2 = em.getReference(Member.class, member2.getId());
+
+            System.out.println("m1 == m2 : " + (m1.getClass() == m2.getClass()));
 
             tx.commit();
         } catch (Exception e) {
